310186e946c768a1150bd6d1f59ffcff3c65be5d
0x1args <stylexload@gmail.com>
2025-04-11 00:11:51 +0300

    Added logic to ValueObject named AuthorBio

--- src/PostIt.Domain/ValueObjects/AuthorBio.cs
+++ src/PostIt.Domain/ValueObjects/AuthorBio.cs
@@ -2,5 +2,26 @@ namespace PostIt.Domain.ValueObjects;
 
 public class AuthorBio
 {
-    
+    public const int MaxLength = 150;
+    public string Value { get; }
+
+    private AuthorBio(string bio)
+    {
+        if(string.IsNullOrWhiteSpace(bio))
+        {
+            throw new ArgumentException("Author bio cannot be empty.", nameof(bio));
+        }
+
+        if (bio.Length > MaxLength)
+        {
+            throw new ArgumentException($"Author bio must be no longer than {MaxLength} characters.", 
+                nameof(bio));
+        }
+        
+        Value = bio;
+    }
+
+    private static AuthorBio Create(string bio) => new(bio);
+
+    public override string ToString() => Value;
 }
