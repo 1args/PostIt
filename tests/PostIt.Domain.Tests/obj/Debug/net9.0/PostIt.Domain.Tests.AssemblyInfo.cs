49f2ff6c704149310116aaa0f7cf32cf2b334406
0x1args <stylexload@gmail.com>
2025-04-20 02:01:43 +0300

    Added docker support

--- tests/PostIt.Domain.Tests/obj/Debug/net9.0/PostIt.Domain.Tests.AssemblyInfo.cs
+++ tests/PostIt.Domain.Tests/obj/Debug/net9.0/PostIt.Domain.Tests.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("PostIt.Domain.Tests")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f4d126f60240ee49b3ed7918b7740102fe5c2a09")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b56420b5656b7f68e8870c44790841bc95c5e4ba")]
 [assembly: System.Reflection.AssemblyProductAttribute("PostIt.Domain.Tests")]
 [assembly: System.Reflection.AssemblyTitleAttribute("PostIt.Domain.Tests")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
